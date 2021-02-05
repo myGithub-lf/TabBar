9d99b9c3655b5d9ddb4398d0c5a34913f703c059
Feng-Lv <feng.lv@aptean.com>
2021-02-05 19:05:37 +0800

    add base structure

---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,41 @@
+<template>
+   <div class="tab-bar-item">
+      <div v-if="!isActive">
+         <slot name="item-icon"></slot>
+      </div>
+      <div v-else>
+         <slot name="item-icon-active"></slot>
+      </div>
+      <div :class="{active: isActive}">
+         <slot name="item-text"></slot>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+   name:"TabBarItem",
+   data() {
+      return {
+         isActive: false
+      }
+   },
+}
+</script>
+
+<style>
+   .tab-bar-item{
+      flex: 1;
+      text-align: center;
+      height: 49px;
+      font-size: 14px;
+   }
+
+   .tab-bar-item img {
+      width: 24px;
+      height: 24px;
+   }
+   .active {
+      color:cornflowerblue;
+   }
+</style>
